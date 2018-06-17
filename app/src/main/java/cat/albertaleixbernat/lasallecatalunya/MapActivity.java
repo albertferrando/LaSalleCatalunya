267c8995e7296bbe7d9646803952366226ce4191
Aleix Baggerman <aleixdiazbaggerman@gmail.com>
2018-06-17 16:51:21 +0200

    [CORE] MapActivity added.

---
+++ app/src/main/java/cat/albertaleixbernat/lasallecatalunya/MapActivity.java
@@ -0,0 +1,13 @@
+package cat.albertaleixbernat.lasallecatalunya;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AleixDiaz on 17/06/2018.
+ */
+
+public class MapActivity extends AppCompatActivity {
+
+    
+
+}
